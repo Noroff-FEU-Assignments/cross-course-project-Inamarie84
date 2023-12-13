52f80869a949dc7674c37087b29d594b4d9da5c8
Inamarie84 <123692666+Inamarie84@users.noreply.github.com>
2023-12-13 13:48:07 +0100

    filtering only female jackets

--- js/script.js
+++ js/script.js
@@ -4,42 +4,26 @@ async function fetchJackets() {
   try {
     const response = await fetch(url);
     const items = await response.json();
-    displayJackets(items);
-  }
-  catch (error) {
+    displayFemaleJackets(items);
+  } catch (error) {
     const jacketContainer = document.querySelector("#featured-products");
     jacketContainer.innerHTML = `<div class="error">Ooops...There was an error fetching the jackets</div>`;
   }
-    
-} 
-
+}
 
 fetchJackets();
 
-// function displayJackets(items) {
-//     const jacketContainer = document.querySelector("#featured-products");
-
-//     jacketContainer.innerHTML= "";
-
-//     items.forEach(function(item) {
-//     jacketContainer.innerHTML += `<div class="product">
-//                                  <img src="${item.image}" alt="Product Image">
-//                                  <h3>${item.title}</h3>
-//                                  <p>${item.description}</p>
-//                                  <div>Sizes: ${item.sizes}</div>
-//                                  <div>Price: ${item.price}</div>
-//                                  <div class="on-sale">On sale: ${item.discountedPrice}</div>
-//                                  <button class="add-cta">Add to cart</button>
-//                                  </div>`  
-//   })
-// }
-
-function displayJackets(items) {
+function displayFemaleJackets(items) {
   const jacketContainer = document.querySelector("#featured-products");
 
   jacketContainer.innerHTML = "";
 
-  items.forEach(function(item) {
+  // Use a for loop to iterate through the items
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+
+    // Check if the item is a female jacket
+    if (item.gender === "Female") {
       // Check if the product is on sale
       const isOnSale = item.onSale;
 
@@ -48,49 +32,121 @@ function displayJackets(items) {
 
       // Build the HTML string with conditional styles
       jacketContainer.innerHTML += `<div class="product">
-                                       <img class="product-images" src="${item.image}" alt="Product Image">
-                                       <h2>${item.title}</h2>
-                                       <p>${item.description}</p>
-                                       <div>Sizes: ${item.sizes}</div>
-                                       <div>
-                                          Price: 
-                                          ${isOnSale
-                                            ? `<span style="color: red; text-decoration: line-through;">$${item.price.toFixed(2)}</span>`
-                                            : `$${item.price.toFixed(2)}`}
-                                       </div>
-                                       ${isOnSale
-                                          ? `<div class="on-sale" style="color: green;">On Sale: $${item.discountedPrice.toFixed(2)}</div>`
-                                          : ''}
-                                       <button class="add-cta">Add to cart</button>
-                                   </div>`;
-  });
+                                     <img class="product-images" src="${item.image}" alt="Product Image">
+                                     <h2>${item.title}</h2>
+                                     <p>${item.description}</p>
+                                     <div>Sizes: ${item.sizes.join(', ')}</div>
+                                     <div>
+                                        Price: 
+                                        ${isOnSale
+                                          ? `<span style="color: red; text-decoration: line-through;">$${item.price.toFixed(2)}</span>`
+                                          : `$${item.price.toFixed(2)}`}
+                                     </div>
+                                     ${isOnSale
+                                        ? `<div class="on-sale" style="color: green;">On Sale: $${item.discountedPrice.toFixed(2)}</div>`
+                                        : ''}
+                                     <button class="add-cta">Add to cart</button>
+                                 </div>`;
+    }
+  }
 }
 
 
+// import { url } from "./constants.js";
+
+// async function fetchJackets() {
+//   try {
+//     const response = await fetch(url);
+//     const items = await response.json();
+//     displayJackets(items);
+//   }
+//   catch (error) {
+//     const jacketContainer = document.querySelector("#featured-products");
+//     jacketContainer.innerHTML = `<div class="error">Ooops...There was an error fetching the jackets</div>`;
+//   }
+    
+// } 
 
 
+// fetchJackets();
 
+// // function displayJackets(items) {
+// //     const jacketContainer = document.querySelector("#featured-products");
 
+// //     jacketContainer.innerHTML= "";
 
-// function displayFemaleJackets(items) {
+// //     items.forEach(function(item) {
+// //     jacketContainer.innerHTML += `<div class="product">
+// //                                  <img src="${item.image}" alt="Product Image">
+// //                                  <h3>${item.title}</h3>
+// //                                  <p>${item.description}</p>
+// //                                  <div>Sizes: ${item.sizes}</div>
+// //                                  <div>Price: ${item.price}</div>
+// //                                  <div class="on-sale">On sale: ${item.discountedPrice}</div>
+// //                                  <button class="add-cta">Add to cart</button>
+// //                                  </div>`  
+// //   })
+// // }
+
+// function displayJackets(items) {
 //   const jacketContainer = document.querySelector("#featured-products");
 
-//   // Filter only female jackets
-//   const femaleJackets = items.filter(item => item.gender === "Female");
+//   jacketContainer.innerHTML = "";
 
-//   femaleJackets.forEach(function(item) {
-//       // Concatenate the HTML string for each female jacket
+//   items.forEach(function(item) {
+//       // Check if the product is on sale
+//       const isOnSale = item.onSale;
+
+
+//       // Check if the product has a discounted price
+//       const hasDiscountedPrice = item.discountedPrice !== undefined;
+
+      
+
+//       // Build the HTML string with conditional styles
 //       jacketContainer.innerHTML += `<div class="product">
-//                                        <img src="${item.image}" alt="${item.title}">
-//                                        <p>${item.title}</p>
+//                                        <img class="product-images" src="${item.image}" alt="Product Image">
+//                                        <h2>${item.title}</h2>
 //                                        <p>${item.description}</p>
-//                                        <p>Price: $${item.price.toFixed(2)}</p>
-//                                      </div>`;
+//                                        <div>Sizes: ${item.sizes}</div>
+//                                        <div>Price: 
+//                                           ${isOnSale
+//                                             ? `<span style="color: red; text-decoration: line-through;">$${item.price.toFixed(2)}</span>`
+//                                             : `$${item.price.toFixed(2)}`}
+//                                        </div>
+//                                        ${isOnSale
+//                                           ? `<div class="on-sale" style="color: green;">On Sale: $${item.discountedPrice.toFixed(2)}</div>`
+//                                           : ''}
+//                                        <button class="add-cta">Add to cart</button>
+//                                    </div>`;
 //   });
 // }
 
-// // Example usage:
-// const items = [/* your array of jackets here */];
 
-// // Call the function with the array of jackets
-// displayFemaleJackets(items);
+
+
+
+
+
+// // function displayFemaleJackets(items) {
+// //   const jacketContainer = document.querySelector("#featured-products");
+
+// //   // Filter only female jackets
+// //   const femaleJackets = items.filter(item => item.gender === "Female");
+
+// //   femaleJackets.forEach(function(item) {
+// //       // Concatenate the HTML string for each female jacket
+// //       jacketContainer.innerHTML += `<div class="product">
+// //                                        <img src="${item.image}" alt="${item.title}">
+// //                                        <p>${item.title}</p>
+// //                                        <p>${item.description}</p>
+// //                                        <p>Price: $${item.price.toFixed(2)}</p>
+// //                                      </div>`;
+// //   });
+// // }
+
+// // // Example usage:
+// // const items = [/* your array of jackets here */];
+
+// // // Call the function with the array of jackets
+// // displayFemaleJackets(items);
