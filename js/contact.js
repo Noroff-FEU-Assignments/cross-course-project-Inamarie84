c3e8a2cfe78aef36de4b4f532eb12d3874205886
Inamarie84 <123692666+Inamarie84@users.noreply.github.com>
2024-01-09 13:31:08 +0100

    added contactjs and form function

---
+++ js/contact.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("form");
+
+function handleSubmit(event) {
+event.preventDefault();
+console.log(event.target);
+
+const form = event.target;
+const formData = new FormData(form);
+const entries = formData.entries();
+const data = Object.fromEntries(entries);
+
+console.log(data);
+}
+
+
+
+form.addEventListener("submit", handleSubmit);
