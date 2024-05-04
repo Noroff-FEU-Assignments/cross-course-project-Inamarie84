3eaf47391408fb3a1d3459a5e82967e2c19f9487
Inamarie84 <123692666+Inamarie84@users.noreply.github.com>
2024-05-04 14:14:19 +0200

    change code render

--- js/ui/products/renderProductThumbnails.js
+++ js/ui/products/renderProductThumbnails.js
@@ -1,50 +1,4 @@
-// export function renderProductThumbnails(targetElement, products) {
-//     const element = document.querySelector(targetElement);
-//     element.innerHTML = "";
-  
-//     const thumbnailHtml = products.map(function (product) {
-//       return createHtmlForProductThumbnail(product);
-//     });
-  
-//     console.log(thumbnailHtml);
-//     element.append(...thumbnailHtml);
-//   }
-
-//   function createHtmlForProductThumbnail(product) {
-//     // nullish coalescing operator
-//     // and optional chaining
-//     const src = product.images?.[0]?.thumbnail || "https://placehold.co/600x400/EEE/31343C";
-//     const alt = product.images?.[0]?.alt || "No image available";
-  
-//     const thumbnailContainer = document.createElement("div");
-//     thumbnailContainer.classList.add("thumbnail");
-
-//     // if using background images
-//     //   const thumbnailContainer = document.createElement("div");
-//     //   thumbnailContainer.classList.add("thumbnail");
-//     //   thumbnailContainer.style.backgroundImage = `url(${src})`;
-  
-//     const img = document.createElement("img");
-//     img.setAttribute("src", src);
-//     img.setAttribute("alt", alt);
-
-//         // Append the thumbnail image to the container
-//         thumbnailContainer.appendChild(img);
-    
-//         // Return the container
-//         return thumbnailContainer;
-   
-  
-//     // const productItem = document.createElement("a");
-//     // productItem.classList.add("product");
-//     // productItem.setAttribute("href", `product.html?id=${id}`);
-//     // const title = document.createElement("h4");
-//     // title.innerText = name;
-//     // productItem.appendChild(title);
-//     // return productItem;
-//   }
-
-  export function renderProductThumbnails(targetElement, products) {
+export function renderProductThumbnails(targetElement, products) {
     const element = document.querySelector(targetElement);
     element.innerHTML = "";
   
@@ -54,20 +8,40 @@
   
     console.log(thumbnailHtml);
     element.append(...thumbnailHtml);
-}
+  }
 
-function createHtmlForProductThumbnail(product) {
-    const src = product.images[0].thumbnail;
-    const alt = product.images[0].alt;
+  function createHtmlForProductThumbnail(product) {
+    // nullish coalescing operator
+    // and optional chaining
+    const src = product.images?.[0]?.thumbnail || "https://placehold.co/600x400/EEE/31343C";
+    const alt = product.images?.[0]?.alt || "No image available";
   
     const thumbnailContainer = document.createElement("div");
     thumbnailContainer.classList.add("thumbnail");
 
+    // if using background images
+    //   const thumbnailContainer = document.createElement("div");
+    //   thumbnailContainer.classList.add("thumbnail");
+    //   thumbnailContainer.style.backgroundImage = `url(${src})`;
+  
     const img = document.createElement("img");
     img.setAttribute("src", src);
     img.setAttribute("alt", alt);
 
-    thumbnailContainer.appendChild(img);
+        // Append the thumbnail image to the container
+        thumbnailContainer.appendChild(img);
     
-    return thumbnailContainer;
-}
+        // Return the container
+        return thumbnailContainer;
+   
+  
+    // const productItem = document.createElement("a");
+    // productItem.classList.add("product");
+    // productItem.setAttribute("href", `product.html?id=${id}`);
+    // const title = document.createElement("h4");
+    // title.innerText = name;
+    // productItem.appendChild(title);
+    // return productItem;
+  }
+
+  
